c234cabd63531a0765a3809d6f0780256f8bc44d
Vijay Ankith <vijay2ankith@gmail.com>
2024-07-12 17:00:02 +0530

    removed dEth token

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -6,31 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-interface iDETH is IERC20 {
-    function mint(address to, uint256 amount) external;
-
-    function burnFrom(address account, uint256 value) external;
-}
-
-contract dEth is iDETH, ERC20, Ownable {
-    constructor() ERC20("dETH", "dETH") Ownable(msg.sender) {}
-
-    function mint(address to, uint256 amount) external override onlyOwner {
-        _mint(to, amount);
-    }
-
-    function burnFrom(address account, uint256 value) external override onlyOwner {
-        _spendAllowance(account, _msgSender(), value);
-        _burn(account, value);
-    }
-
-    function _transfer(address from, address to, uint256 amount) internal override {
-        revert("dETH: transfers are disabled");
-    }
-}
-
 contract Staking is AccessControl {
-    iDETH public dEthToken;
     bytes32 public constant LIQUIDITY_MANAGER_ROLE = keccak256("LIQUIDITY_MANAGER_ROLE");
 
     mapping(address => uint256) public stakedBalances;
@@ -44,7 +20,6 @@ contract Staking is AccessControl {
     event LiquidityTransferred(address indexed user, uint256 amount);
 
     constructor() {
-        dEthToken = new dEth();
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(LIQUIDITY_MANAGER_ROLE, msg.sender);
     }
@@ -57,7 +32,6 @@ contract Staking is AccessControl {
     function stake() external payable {
         require(msg.value > 0, "Cannot stake 0 ETH");
         stakedBalances[msg.sender] += msg.value;
-        dEthToken.mint(msg.sender, msg.value);
         // Emit Staked event
         emit Staked(msg.sender, msg.value);
     }
@@ -73,10 +47,7 @@ contract Staking is AccessControl {
     }
 
     function withdraw(uint256 _amount) external {
-        require(dEthToken.balanceOf(msg.sender) >= _amount, "Insufficient dETH balance");
         require(block.timestamp >= unstakeTimestamps[msg.sender] + COOLDOWN_PERIOD, "Cooldown period not yet passed");
-
-        dEthToken.burnFrom(msg.sender, _amount);
         payable(msg.sender).transfer(_amount);
 
         // Emit Withdrawn event
